f13b4353ed9ef27a7d67fe805ed34ca5d6ab2d72
Hyunjin Jung <hjjung0810@naver.com>
2022-05-14 14:12:36 +0900

    delete activity_posting2.xml

--- app/src/main/java/OSS/geteatwithme/PostingActivity.java
+++ app/src/main/java/OSS/geteatwithme/PostingActivity.java
@@ -9,6 +9,6 @@ public class PostingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_posting2);
+        setContentView(R.layout.activity_posting);
     }
 }
