ec733fcb83d78caae2a1650a76adad1115bc5921
Hyunjin Jung <hjjung0810@naver.com>
2022-05-14 18:49:06 +0900

    modify MainActivity
    - show pots

--- app/src/main/java/OSS/geteatwithme/MainActivity.java
+++ app/src/main/java/OSS/geteatwithme/MainActivity.java
@@ -4,24 +4,56 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.ScrollView;
 import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.material.chip.ChipGroup;
 
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.ListIterator;
 
 import OSS.geteatwithme.PostInfo.Post;
 
 public class MainActivity extends AppCompatActivity {
     RadioButton[] radioButtons = new RadioButton[8];
+    LinkedList<Post> posts = new LinkedList<Post>();
+    LinearLayout linearlayout = null;
 
     void setAllRadioButtonOff(){
         for(int i=0; i< 8; i++)
             radioButtons[i].setChecked(false);
+    }
+
+    void showPosts(){
+        if(linearlayout != null)
+            ((ViewGroup)linearlayout.getParent()).removeView(linearlayout);
+        linearlayout = new LinearLayout(this);
+        linearlayout.setOrientation(LinearLayout.VERTICAL);
+
+        // test-
+        int idx = 0;
+        for(Post p : posts){
+            Button b = new Button(this);
+            b.setLayoutParams(new androidx.appcompat.app.ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT ));
+            b.setText(p.getRestaurant());
+            linearlayout.addView(b, idx++);
+        }
+        // -test
+
+        ScrollView view = (ScrollView)findViewById(R.id.scrollView2);
+        view.addView(linearlayout, 0);
+
     }
 
     @Override
@@ -38,6 +70,10 @@ public class MainActivity extends AppCompatActivity {
         radioButtons[6] = (RadioButton) findViewById(R.id.radioButton18);
         radioButtons[7] = (RadioButton) findViewById(R.id.radioButton19);
 
+        // 전체 post 보여주기
+        // posts <- 전체 포스트
+        showPosts();
+
         // 한식
         radioButtons[0].setOnClickListener(new View.OnClickListener(){
 
@@ -45,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 setAllRadioButtonOff();
                 radioButtons[0].setChecked(true);
+
             }
         });
 
