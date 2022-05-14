fe59f659657c5e276a6080fff5ef2f1276c855b0
Hyunjin Jung <hjjung0810@naver.com>
2022-05-14 14:11:07 +0900

    add PostingActivity

---
+++ app/src/main/java/OSS/geteatwithme/PostingActivity.java
@@ -0,0 +1,14 @@
+package OSS.geteatwithme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_posting2);
+    }
+}
